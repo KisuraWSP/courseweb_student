8ed11ccdc1a532ba6ed5d93287a622f95b81da80
IT21803420 <it21803420@my.sliit.lk>
2023-10-28 23:22:56 +0530

    Added activity kotlin files and xml layouts for activities

---
+++ app/src/main/java/com/example/courseweb_student/AdminActivity.kt
@@ -0,0 +1,10 @@
+package com.example.courseweb_student
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AdminActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
